0a87ad1fe0cf072cd00e0023ba4f821374ec83b2
Justin Hill <jhill.d@gmail.com>
2013-03-17 17:15:59 -0400

    - Working on "Add another" sliding in editor for part two

--- webroot/js/main.js
+++ webroot/js/main.js
@@ -1,35 +1,102 @@
 var khan_academy = (function () {
 
 	document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')
+	
+	// utility functions
+	var util = (function () {
+		
+		var publicMembers = {
+			formattedTimeWithSeconds: function () {
+				var stringTime, hrs, mins, secs;
 
-	var formattedTimeWithSeconds = function () {
-		var stringTime, hrs, mins, secs;
+				stringTime = "";
+				hrs = Math.floor(seconds / 3600);
+				mins = Math.floor((seconds - (hrs * 3600)) / 60);
+				secs = Math.floor(seconds - (hrs * 3600) - (mins * 60));
 
-		stringTime = "";
-		hrs = Math.floor(seconds / 3600);
-		mins = Math.floor((seconds - (hrs * 3600)) / 60);
-		secs = Math.floor(seconds - (hrs * 3600) - (mins * 60));
+				if (hrs > 0) {
+					stringTime += hrs + ":";
+				}
 
-		if (hrs > 0) {
-			stringTime += hrs + ":";
+				if (mins < 10 && hrs > 0) {
+					stringTime += "0" + mins + ":";
+				} else {
+					stringTime += mins + ":";
+				}
+
+				if (secs < 10) {
+					stringTime += "0" + secs;
+				} else {
+					stringTime += secs;
+				}
+
+				return stringTime;
+			},
+			generateGuid: function () {
+				// courtesy of http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
+				'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+				    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+				    return v.toString(16);
+				});
+			}
+		};
+
+		return publicMembers;
+	})();
+
+	var editor_step2 = (function () {
+
+		// private members
+		var negMarginHeight = 0;
+
+		// self refers to the element, not this object.
+		function addAnother(self)
+		{
+			var newId = util.generateGuid();
+			var oldQ = $('.question:last');
+			var newQ = oldQ.clone(false);
+			newQ.id = newId;
+			$(newQ).css("margin-top", -($(oldQ).outerHeight()));
+
+			$('.question').each(function () {
+				$(this).transition({x: -($(this).width() + 15), "opacity": .3}, 100, function () {
+					var m = $(this).css("margin-left");
+					console.log("m: " + m );
+					console.log("m-width+15: " + (parseInt(m) - $(this).width() + 15));
+					$(this).removeAttr('style');
+					$(this).css('opacity', .3)
+					$(this).css("left", -($(this).width() + 15));
+					console.log('callback!');
+				});
+				console.log(this.id + ": " + $(this).css("left"));
+			});
+
+			$('#step2_editor').append(newQ);
 		}
 
-		if (mins < 10 && hrs > 0) {
-			stringTime += "0" + mins + ":";
-		} else {
-			stringTime += mins + ":";
+		// public functions
+
+		var publicMembers = {
+
 		}
 
-		if (secs < 10) {
-			stringTime += "0" + secs;
-		} else {
-			stringTime += secs;
-		}
+		// on document ready
+		$(function () {
+			$('#step2_editor').on('click', '.addAnother', function () {
+				addAnother(this);
+				console.log("called");
+			});
 
-		return stringTime;
-	}
+			var guid = util.generateGuid();
+			$('.section_step2').attr('id', guid);
+		});
+	})();
+
+
+	// on document ready
 	$(function () {
 
+		// on video.js ready
 		_V_(video_player).ready(function () {
 			var thePlayer = this;
 			var lastTimeUpdate = 0;
