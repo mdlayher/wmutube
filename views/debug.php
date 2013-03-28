24654ab353d056f0fba635c8ba2668a215405928
Matt Layher <mdlayher@gmail.com>
2013-03-27 21:16:17 -0400

    Added seltest to debug

--- views/debug.php
+++ views/debug.php
@@ -3,11 +3,31 @@
 ?>
 <div id="body_container">
 	<span id="error"></span>
-	<div class="centered_on_page">Session Dump</h1></div>
-	<div>
+	<div style="margin-left: 25%">
 		<pre>
+		Session Dump
 <?= print_r($_SESSION, true); ?>
 		</pre>
+		<pre>
+		Cache Version
+
+<?= print_r(cache::version(), true); ?>
+		</pre>
+		<a href="debug?selftest=1">Selftest</a>
+		<?php
+			if (isset($_GET['selftest']) && $_GET['selftest'])
+			{
+				config::load(array("user", "video", "course", "question", "answer", "role"));
+				echo "<pre>\n";
+				answer::selftest();
+				course::selftest();
+				question::selftest();
+				role::selftest();
+				user::selftest();
+				video::selftest();
+				echo "</pre>\n";
+			}
+		?>
 	</div>
 </div>
 <?php
