796a88744ae2cd791b9f0a756d21e37eec023513
Justin Hill <jhill.d@gmail.com>
2013-03-31 16:06:44 -0400

    - Added videos view - Added videos view to index.php (endpoint /videos)

---
+++ views/videos.php
@@ -0,0 +1,14 @@
+<?php
+	require_once "header.inc.php";
+?>
+<link rel="stylesheet" type="text/css" href="header.css">
+<script src="./js/jquery.scrollTo.js"></script>
+<script src="./js/videoList.js" type="text/javascript"></script>
+<div id="body_container">
+
+
+
+</div>
+<?php
+	require_once "footer.inc.php";
+?>
