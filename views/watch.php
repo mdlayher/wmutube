489a0abcd75d4bb52939dbf76116ca85cd05ecb2
Matt Layher <mdlayher@gmail.com>
2013-04-08 19:34:58 -0300

    Add 'error' span

--- views/watch.php
+++ views/watch.php
@@ -1,15 +1,12 @@
 <?php
 	require_once "header.inc.php";
 ?>
-
 <link href="http://vjs.zencdn.net/c/video-js.css" rel="stylesheet">
 <script src="http://vjs.zencdn.net/c/video.js"></script>
-
 <link rel="stylesheet" type="text/css" href="<?= $root_uri ?>/css/watch.css">
 <script src="<?= $root_uri ?>/js/jquery.scrollTo.js"></script>
 <script type="text/javascript">var questions = <?php $q = $video->to_array(); echo json_encode($q["questions"]); ?>;</script>
 <script src="<?= $root_uri ?>/js/watch.js" type="text/javascript"></script>
-
 <div class="body_fade"></div>
 <div class="quiz_container">
 	<div class="quiz_result incorrect">
@@ -29,8 +26,8 @@
 		<div id="quiz_submit" class="button right">Submit</div>
 	</footer>
 </div>
-
 <div id="body_container">
+	<span id="error"></span>
 	<div class="player_container centered_on_page">
 		<h1><header><?= $video->get_title() ?></header></h1>
 		<video class="video-js vjs-default-skin" data-setup="{}" id="video_player" autoplay poster="<?= $root_uri ?>/img/player_placeholder.png" preload="auto" controls>
