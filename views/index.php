5503440e7cc4f55070439372ffc9c52ec9387e73
Matt Layher <mdlayher@gmail.com>
2013-05-02 09:30:05 -0400

    Update index

--- views/index.php
+++ views/index.php
@@ -12,8 +12,12 @@
 		"<a href="http://en.wikipedia.org/wiki/Flip_teaching">Inverted Classroom</a>" learning system for the
 		<a href="http://cs.wmich.edu">Computer Science Department</a> at <a href="http://wmich.edu">Western Michigan University.</a></p>
 
-		<p>This project is still under heavy development, but feel free to check back for updates!  The final system
-		will be live by the end of April 2013!</p>
+		<p>WMU students: To get started, you may log in using your Bronco NetID and password.  From here, you can view
+		any video on the site, and take interactive quizzes during each video.  Your scores will be recorded in order
+		to better aid your instructors during future classes!</p>
+
+		<p>WMU staff and faculty: To get started, please contact <a href="mailto:matthew.d.layher@wmich.edu">Matt Layher</a>
+		to have your account created.  From here, you may upload videos and create interactive quizzes for your students.</a>
 	</div>
 </div>
 <?php
